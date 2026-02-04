788cfc28288fabcf7355521b7848247a2cc65ea9
setsuna0402 <mayuyuqueen@gmail.com>
2026-02-04 22:08:30 +0800

    updated v1 for seperating model definition from it

--- NTU_ML_2021/HW_3_CNN_image_classification/Classificator_CNN_v1.py
+++ NTU_ML_2021/HW_3_CNN_image_classification/Classificator_CNN_v1.py
@@ -21,6 +21,8 @@ from torchvision.datasets import DatasetFolder
 
 # This is for the progress bar.
 from tqdm.auto import tqdm
+import time
+from Model_Class import Classifier
 
 allow_device = True  # Set to False if you want to force using CPU.
 use_pin_memory = False  # Set to True if you use GPU. False for CPU and MPS.
@@ -61,51 +63,7 @@ train_loader = DataLoader(train_set, batch_size=batch_size, shuffle=True, num_wo
 valid_loader = DataLoader(valid_set, batch_size=batch_size, shuffle=True, num_workers=0, pin_memory=use_pin_memory)
 test_loader = DataLoader(test_set, batch_size=batch_size, shuffle=False)
 
-class Classifier(nn.Module):
-    def __init__(self):
-        super(Classifier, self).__init__()
-        # The arguments for commonly used modules:
-        # torch.nn.Conv2d(in_channels, out_channels, kernel_size, stride, padding)
-        # torch.nn.MaxPool2d(kernel_size, stride, padding)
 
-        # input image size: [3, 128, 128]
-        self.cnn_layers = nn.Sequential(
-            nn.Conv2d(3, 64, 3, 1, 1),
-            nn.BatchNorm2d(64),
-            nn.ReLU(),
-            nn.MaxPool2d(2, 2, 0),
-
-            nn.Conv2d(64, 128, 3, 1, 1),
-            nn.BatchNorm2d(128),
-            nn.ReLU(),
-            nn.MaxPool2d(2, 2, 0),
-
-            nn.Conv2d(128, 256, 3, 1, 1),
-            nn.BatchNorm2d(256),
-            nn.ReLU(),
-            nn.MaxPool2d(4, 4, 0),
-        )
-        self.fc_layers = nn.Sequential(
-            nn.Linear(256 * 8 * 8, 256),
-            nn.ReLU(),
-            nn.Linear(256, 256),
-            nn.ReLU(),
-            nn.Linear(256, 11)
-        )
-
-    def forward(self, x):
-        # input (x): [batch_size, 3, 128, 128]
-        # output: [batch_size, 11]
-
-        # Extract features by convolutional layers.
-        x = self.cnn_layers(x)
-
-        # The extracted feature map must be flatten before going to fully-connected layers.
-        x = x.flatten(1)
-
-        # The features are transformed by fully-connected layers to obtain the final logits.
-        x = self.fc_layers(x)
-        return x
 
 # This func doesn't be used in version 0.1. It is designed for semi-supervised learning.    
 def get_pseudo_labels(dataset, model, threshold=0.65):
@@ -176,6 +134,8 @@ n_epochs = 80
 # Whether to do semi-supervised learning.
 do_semi = False
 
+# start time record
+start_time = time.time()
 for epoch in range(n_epochs):
     # ---------- TODO ----------
     # In each epoch, relabel the unlabeled dataset for semi-supervised learning.
@@ -272,3 +232,16 @@ valid_acc = sum(valid_accs) / len(valid_accs)
 # Print the information.
 # print(f"[ Valid | {epoch + 1:03d}/{n_epochs:03d} ] loss = {valid_loss:.5f}, acc = {valid_acc:.5f}")
 print(f"[ Valid ] loss = {valid_loss:.5f}, acc = {valid_acc:.5f}")
+
+# end time record
+end_time = time.time()
+print("Training time: {:.2f} minutes".format((end_time - start_time)/60))
+# Save the trained model.
+model.eval()
+model.to(torch.device("cpu"))  # Move the model to CPU before saving.
+model_dict = { 
+            "classifier_network" : model.state_dict(),
+            "classifier_optimizer" : optimizer.state_dict(),
+        }   
+torch.save(model_dict, "classifier_cnn_v1_batch_{}_epoch_{}.pth".format(batch_size, n_epochs))
+print("Model saved.")
