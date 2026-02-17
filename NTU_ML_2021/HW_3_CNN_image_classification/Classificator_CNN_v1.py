42ec983f39930a9b05f83ffd828ebd68811a36b8
setsuna0402 <mayuyuqueen@gmail.com>
2026-02-17 21:56:44 +0800

    support CutMix

--- NTU_ML_2021/HW_3_CNN_image_classification/Classificator_CNN_v1.py
+++ NTU_ML_2021/HW_3_CNN_image_classification/Classificator_CNN_v1.py
@@ -14,20 +14,24 @@ import numpy as np
 import torch
 import torch.nn as nn
 import torchvision.transforms as transforms
+from torchvision.transforms import v2
 from PIL import Image
 # "ConcatDataset" and "Subset" are possibly useful when doing semi-supervised learning.
 from torch.utils.data import ConcatDataset, DataLoader, Subset
 from torchvision.datasets import DatasetFolder
+import matplotlib.pyplot as plt
 
 # This is for the progress bar.
 from tqdm.auto import tqdm
 import time
 from Model_Class import Classifier
+# from Transform_func import *
 
 allow_device = True  # Set to False if you want to force using CPU.
 use_pin_memory = True  # Set to True if you use GPU. False for CPU and MPS.
-n_epochs = 80 # The number of training epochs for classifier. 
-do_semi = False # Whether to do semi-supervised learning.
+n_epochs = 160 # The number of training epochs for classifier.
+do_cutmix = True # Apply cut mix method
+cutmix_alpha = 1.0 
 # Batch size for training, validation, and testing.
 # A greater batch size usually gives a more stable gradient.
 # But the GPU memory is limited, so please adjust it carefully.
@@ -36,9 +40,11 @@ batch_size = 128
 # If you use cuda, you may set it to a greater value like 4 or 8 to accelerate data loading.
 num_workers = 8  # You may change this value based on your system configuration.
 file_path = "./project_data_food_11" # Used to load data
+
 # It is important to do data augmentation in training.
 # However, not every augmentation is useful.
 # Please think about what kind of augmentation is helpful for food recognition.
+'''
 train_tfm = transforms.Compose([
     # Resize the image into a fixed shape (height = width = 128)
     transforms.Resize((128, 128)),
@@ -57,6 +63,38 @@ test_tfm = transforms.Compose([
     transforms.ToTensor(),
     transforms.Normalize(mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5]), # Normalize the image with mean and std of ImageNet dataset.
 ])
+'''
+train_transform = transforms.Compose([
+    # Resize the image into a fixed shape (height = width = 128)
+    transforms.Resize((156, 156)),
+    # You may add some transforms here.
+    # ToTensor() should be the last one of the transforms.
+    
+    # transforms.RandomChoice(
+    #     [transforms.AutoAugment(),
+    #     transforms.AutoAugment(transforms.AutoAugmentPolicy.CIFAR10),
+    #     transforms.AutoAugment(transforms.AutoAugmentPolicy.SVHN)]
+    # ),
+    transforms.RandomResizedCrop((128, 128), scale=(0.5, 1)),
+    # transforms.ColorJitter(0.2, 0.2, 0.2, 0.05),
+    transforms.ColorJitter(0.2, 0.2),
+    transforms.RandomHorizontalFlip(0.5),
+    transforms.RandomRotation(15),
+    transforms.ToTensor(),
+    transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]), # Normalize the image with mean and std of ImageNet dataset.
+])
+
+# We don't need augmentations in testing and validation.
+# All we need here is to resize the PIL image and transform it into Tensor.
+test_transform = transforms.Compose([
+    transforms.Resize((156, 156)),
+    transforms.CenterCrop(128),
+    transforms.ToTensor(),
+    transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]), # Normalize the image with mean and std of ImageNet dataset.
+])
+
+train_tfm = train_transform
+test_tfm = test_transform
 
 # Construct datasets.
 # The argument "loader" tells how torchvision reads the data.
@@ -70,43 +108,6 @@ train_loader = DataLoader(train_set, batch_size=batch_size, shuffle=True, num_wo
 valid_loader = DataLoader(valid_set, batch_size=batch_size, shuffle=True, num_workers=num_workers, pin_memory=use_pin_memory)
 test_loader = DataLoader(test_set, batch_size=batch_size, shuffle=False)
 
-
-
-# This func doesn't be used in version 0.1. It is designed for semi-supervised learning.    
-def get_pseudo_labels(dataset, model, threshold=0.65):
-    # This functions generates pseudo-labels of a dataset using given model.
-    # It returns an instance of DatasetFolder containing images whose prediction confidences exceed a given threshold.
-    # You are NOT allowed to use any models trained on external data for pseudo-labeling.
-    device = "cuda" if torch.cuda.is_available() else "cpu"
-
-    # Construct a data loader.
-    data_loader = DataLoader(dataset, batch_size=batch_size, shuffle=False)
-
-    # Make sure the model is in eval mode.
-    model.eval()
-    # Define softmax function.
-    # In Pytorch, cross-entropy loss function already includes softmax operation.
-    # So, we don't define softmax in the model. We define it here for obtaining probability distributions.
-    softmax = nn.Softmax(dim=-1) 
-
-    # Iterate over the dataset by batches.
-    for batch in tqdm(data_loader):
-        img, _ = batch
-
-        # Forward the data
-        # Using torch.no_grad() accelerates the forward process.
-        with torch.no_grad():
-            logits = model(img.to(device)) # logits: [batch_size, n_classes] No gradient calculation
-
-        # Obtain the probability distributions by applying softmax on logits.
-        probs = softmax(logits)
-
-        # ---------- TODO ----------
-        # Filter the data and construct a new dataset.
-
-    # # Turn off the eval mode.
-    model.train()
-    return dataset
 
 # Automatically choose the device to use.
 if allow_device:
@@ -130,26 +131,23 @@ model = Classifier().to(device)
 model.device = device
 
 # For the classification task, we use cross-entropy as the measurement of performance.
-criterion = nn.CrossEntropyLoss()
+criterion = nn.CrossEntropyLoss(label_smoothing=0.05)
+# For Cut Mix
+if do_cutmix:
+    cutmix = v2.CutMix(alpha=cutmix_alpha, num_classes=11)
 
 # Initialize optimizer, you may fine-tune some hyperparameters such as learning rate on your own.
-optimizer = torch.optim.Adam(model.parameters(), lr=0.0003, weight_decay=1e-5)
+optimizer = torch.optim.AdamW(model.parameters(), lr=0.0003, weight_decay=1e-3)
+# optimizer = torch.optim.SGD(model.parameters(), lr=0.03, momentum=0.9, weight_decay=1e-3) # Resnet is deep. Use SGD to accelerate the training process
+# This is for SGD. Dynamically decay the lr. 
+scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, T_max=n_epochs)
+
+# Record validation accuracy across epochs
+valid_acc_epoch = []
 
 # start time record
 start_time = time.time()
 for epoch in range(n_epochs):
-    # ---------- TODO ----------
-    # In each epoch, relabel the unlabeled dataset for semi-supervised learning.
-    # Then you can combine the labeled dataset and pseudo-labeled dataset for the training.
-    if do_semi:
-        # Obtain pseudo-labels for unlabeled data using trained model.
-        pseudo_set = get_pseudo_labels(unlabeled_set, model)
-
-        # Construct a new dataset and a data loader for training.
-        # This is used in semi-supervised learning only.
-        concat_dataset = ConcatDataset([train_set, pseudo_set])
-        train_loader = DataLoader(concat_dataset, batch_size=batch_size, shuffle=True, num_workers=num_workers, pin_memory=use_pin_memory)
-
     # ---------- Training ----------
     # Make sure the model is in train mode before training.
     model.train()
@@ -163,13 +161,20 @@ for epoch in range(n_epochs):
 
         # A batch consists of image data and corresponding labels.
         imgs, labels = batch
+        # Send imgs and labels to device
+        imgs = imgs.to(device, non_blocking=True)
+        labels = labels.to(device, non_blocking=True)
 
+        # Perform cut mix
+        if do_cutmix:
+            imgs, labels = cutmix(imgs, labels)
+        
         # Forward the data. (Make sure data and model are on the same device.)
-        logits = model(imgs.to(device))
+        logits = model(imgs)
 
         # Calculate the cross-entropy loss.
         # We don't need to apply softmax before computing cross-entropy as it is done automatically.
-        loss = criterion(logits, labels.to(device))
+        loss = criterion(logits, labels)
 
         # Gradients stored in the parameters in the previous step should be cleared out first.
         optimizer.zero_grad()
@@ -183,19 +188,59 @@ for epoch in range(n_epochs):
         # Update the parameters with computed gradients.
         optimizer.step()
 
+        if not do_cutmix:
+            # Compute the accuracy for current batch.
+            acc = (logits.argmax(dim=-1) == labels).float().mean()
+            train_accs.append(acc)
+
+        # Record the loss and accuracy.
+        train_loss.append(loss.item())
+
+    # The average loss and accuracy of the training set is the average of the recorded values.
+    train_loss = sum(train_loss) / len(train_loss)
+    if do_cutmix:
+        # Print the information.
+        print("Training with CutMix, so we don't calculate the training accuracy.")
+        print(f"[ Train | {epoch + 1:03d}/{n_epochs:03d} ] loss = {train_loss:.5f}")
+    else:
+        train_acc = sum(train_accs) / len(train_accs)
+        print(f"[ Train | {epoch + 1:03d}/{n_epochs:03d} ] loss = {train_loss:.5f}, acc = {train_acc:.5f}")
+
+    model.eval()
+    # These are used to record information in validation.
+    valid_loss = []
+    valid_accs = []
+
+    # Iterate the validation set by batches.
+    for batch in tqdm(valid_loader):
+
+        # A batch consists of image data and corresponding labels.
+        imgs, labels = batch
+
+        # We don't need gradient in validation.
+        # Using torch.no_grad() accelerates the forward process.
+        with torch.no_grad():
+            logits = model(imgs.to(device))
+
+        # We can still compute the loss (but not the gradient).
+        loss = criterion(logits, labels.to(device))
+
         # Compute the accuracy for current batch.
         acc = (logits.argmax(dim=-1) == labels.to(device)).float().mean()
 
         # Record the loss and accuracy.
-        train_loss.append(loss.item())
-        train_accs.append(acc)
+        valid_loss.append(loss.item())
+        valid_accs.append(acc.item())
 
-    # The average loss and accuracy of the training set is the average of the recorded values.
-    train_loss = sum(train_loss) / len(train_loss)
-    train_acc = sum(train_accs) / len(train_accs)
+    scheduler.step() # update learning rate
+    current_lr = optimizer.param_groups[0]["lr"]
 
+    # The average loss and accuracy for entire validation set is the average of the recorded values.
+    valid_loss = sum(valid_loss) / len(valid_loss)
+    valid_acc = sum(valid_accs) / len(valid_accs)
+    valid_acc_epoch.append(valid_acc)
     # Print the information.
-    print(f"[ Train | {epoch + 1:03d}/{n_epochs:03d} ] loss = {train_loss:.5f}, acc = {train_acc:.5f}")
+    print(f"[ Valid | {epoch + 1:03d}/{n_epochs:03d} ] loss = {valid_loss:.5f}, acc = {valid_acc:.5f}, lr = {current_lr:.5f}")
 
 # ---------- Validation ----------
 # Make sure the model is in eval mode so that some modules like dropout are disabled and work normally.
@@ -237,6 +282,17 @@ print(f"[ Valid ] loss = {valid_loss:.5f}, acc = {valid_acc:.5f}")
 # end time record
 end_time = time.time()
 print("Training time: {:.2f} minutes".format((end_time - start_time)/60))
+
+# Convert valid_acc_epoch to numpy array for making plot
+valid_acc_epoch = np.array(valid_acc_epoch)
+plt.plot(np.arange(n_epochs), valid_acc_epoch, "b-", label="Validation")
+plt.xlabel("Epoch")
+plt.ylabel("Accuracy")
+plt.savefig("classifier_v1_accuracy_batch_{}_epoch_{}_adamW_cutmix.png".format(batch_size, n_epochs))
+plt.close()
+
+
+# exit()
 # Save the trained model.
 model.eval()
 model.to(torch.device("cpu"))  # Move the model to CPU before saving.
@@ -244,5 +300,5 @@ model_dict = {
             "classifier_network" : model.state_dict(),
             "classifier_optimizer" : optimizer.state_dict(),
         }   
-torch.save(model_dict, "classifier_cnn_v1_batch_{}_epoch_{}.pth".format(batch_size, n_epochs))
+torch.save(model_dict, "classifier_cnn_v1_batch_{}_epoch_{}_adamW_cutmix.pth".format(batch_size, n_epochs))
 print("Model saved.")
